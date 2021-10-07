0b34bc4b38e16a59dc86e18bb7c92686f61958c3
Qiaoliwang2020 <414217795zoe@gmail.com>
2021-10-07 19:04:31 +1100

    change the db connection

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // MongoURI: 'mongodb+srv://dbuser1:dbuser1@cluster0.9lzkt.mongodb.net/online-doctor?retryWrites=true&w=majority',
-    MongoURI:'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+    MongoURI: 'mongodb+srv://dbuser1:dbuser1@cluster0.9lzkt.mongodb.net/online-doctor?retryWrites=true&w=majority',
+    // MongoURI:'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
     MAIL_ID: 'demomikewang@gmail.com',
     MAIL_PASS: 'DeakinSIT'
  }
